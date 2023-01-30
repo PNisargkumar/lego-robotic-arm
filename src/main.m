fed5b729ce3dbd7e410ee59a1640e69d62ebad0c
Nayan Savaliya <48629002+nayansavaliya@users.noreply.github.com>
2023-01-30 17:52:06 +0100

    fixes merge issues

--- src/main.m
+++ src/main.m
@@ -1,11 +1,8 @@
 clear;
 clc;
 mylego = legoev3('usb');
-<<<<<<< HEAD
-=======
 % x & y co-orcinates of the flatforms
 % heights of the platforms are variable and measured 
->>>>>>> 3ad755b3df2dc63f2967a76170479bbfea6f9e35
 ax = 117.9;
 ay = 0;
 bx = 0;
@@ -18,7 +15,6 @@ disp('Going to Home position')
 robo.zuHause()
 disp('Reading Platform Heights')
 robo.hoehelesen();
-<<<<<<< HEAD
 disp('Performing Inverse kinematics using geometical approach')
 robo.invKinT2();
 disp('Performing action 5  -- B -> C')
@@ -32,19 +28,4 @@ acht(robo);
 disp('Performing action 9  -- A -> C')
 neun(robo);
 disp('Performing action 10 -- C -> A')
-=======
-disp('Performing Invverse kinematics using geometical approach')
-robo.invKinT2();
-disp('Performing action 5')
-funf(robo);
-disp('Performing action 6')
-sechs(robo);
-disp('Performing action 7')
-sieben(robo);
-disp('Performing action 8')
-acht(robo);
-disp('Performing action 9')
-neun(robo);
-disp('Performing action 10')
->>>>>>> 3ad755b3df2dc63f2967a76170479bbfea6f9e35
 zehn(robo);
